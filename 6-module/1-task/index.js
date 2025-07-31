fde9f0414b78aa59ea7dcc34936b708de05a1bb5
Korchgaina <korchagina-1995@inbox.ru>
2025-07-31 11:43:10 +0300

    Реализация класса таблицы с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,54 @@
  *      }
  *
  */
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.renderTable();
   }
+
+  renderTable() {
+    const table = document.createElement('table');
+
+    const thead = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+        </tr>
+      </thead>
+    `;
+
+    const tbodyRows = this.rows.map(row => `
+      <tr>
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      </tr>
+    `).join('');
+
+    const tbody = `<tbody>${tbodyRows}</tbody>`;
+
+    table.innerHTML = thead + tbody;
+
+    table.addEventListener('click', (e) => {
+
+      const target = e.target;
+
+      if (target.tagName === 'BUTTON') {
+        target.closest('tr').remove();
+      }
+    });
+
+    return table;
+  }
+  
 }
+
+
+
